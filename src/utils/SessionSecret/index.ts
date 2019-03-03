6d567811dd95e641bc5e5c709cbafda8bb1a9677
gsouders17 <gsouders17@gmail.com>
2019-03-03 17:41:42 -0500

    session secret

---
+++ src/utils/SessionSecret/index.ts
@@ -0,0 +1,10 @@
+import * as config from "config";
+import { createHmac } from "crypto";
+
+export function getSessionSecret() {
+  const hash = createHmac('sha512', config.get("session.salt"));
+
+  hash.update(process.env.npm_package_version);
+
+  return hash.digest('hex');
+}
