d855b09ed60acff34e5bbfdf33cbd48f3ac923b4
gsouders17 <gsouders17@gmail.com>
2019-03-03 16:23:14 -0500

    added caretaker router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,13 @@ import { PatientRouter } from './Patient/patient-router';
 import { ScheduleRouter } from './Schedule/schedule-router';
 import { NoteRouter } from './Note/note-router';
 import { AuthRouter } from './Auth/auth-router';
-
+import { CaretakerRouter } from './Caretaker/caretaker-router';
 
 export default [
   PatientRouter,
   UserRouter,
   ScheduleRouter,
   NoteRouter,
-  AuthRouter
+  AuthRouter,
+  CaretakerRouter
 ];
