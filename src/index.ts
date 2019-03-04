fb6c165174c8c81b8ecd39919bb4d288243e2b2f
gsouders17 <gsouders17@gmail.com>
2019-03-03 19:58:37 -0500

    added database connection

--- src/index.ts
+++ src/index.ts
@@ -8,69 +8,68 @@ import * as fastifyCookie from "fastify-cookie";
 import * as fastifySwagger from "fastify-swagger";
 import * as fastifySession from "fastify-session";
 import { getSessionSecret } from "./utils/SessionSecret";
-// import { createConnection } from 'typeorm';
+import { createConnection } from 'typeorm';
 
 import routes from './routes';
 
-const main = async () => {
-  const fastify = Fastify({ 
-    logger: config.get("server.logger")
-  });
+createConnection().then(async (connection) => {
+  const main = async () => {
+    const fastify = Fastify({
+      logger: config.get("server.logger")
+    });
 
-  fastify.register(fastifySwagger, {
-    exposeRoute: true,
-    swagger: {
-      consumes: ['application/json'],
-      info: {
-        description: 'Documentation for the CareLine API',
-        title: 'CareLine API',
-        version: '0.0.1'
+    fastify.register(fastifySwagger, {
+      exposeRoute: true,
+      swagger: {
+        consumes: ['application/json'],
+        info: {
+          description: 'Documentation for the CareLine API',
+          title: 'CareLine API',
+          version: '0.0.1'
+        },
+        produces: ['application/json'],
+        schemes: ['http'],
       },
-      produces: ['application/json'],
-      schemes: ['http'],
-    },
-  });
+    });
 
-  const redisStore = RedisStore(fastifySession);
+    const redisStore = RedisStore(fastifySession);
 
-  const sessionOptions: any = {
-    cookie: {
-      secure: false
-    },
-    secret: getSessionSecret(),
-    store: new redisStore(config.get("redis"))
+    const sessionOptions: any = {
+      cookie: {
+        secure: false
+      },
+      secret: getSessionSecret(),
+      store: new redisStore(config.get("redis"))
+    };
+
+    fastify.register(fastifyCORS, config.get("cors"));
+    fastify.register(fastifyCookie);
+    fastify.register(fastifySession, sessionOptions);
+
+    try {
+      fastify.after(() => {
+        routes.forEach(Route => new Route(fastify));
+      });
+
+      fastify.listen(config.get("server.port"), "0.0.0.0", (err ? : Error) => {
+        if (err) {
+          fastify.log.error(err);
+
+          return process.exit(1);
+        }
+
+        fastify.log.info(
+          `Server is listening on port ${config.get("server.port")}`
+        );
+      });
+    } catch (err) {
+      fastify.log.info('Failed to start server');
+
+      process.exit(1);
+    }
+
   };
-
-  fastify.register(fastifyCORS, config.get("cors"));
-  fastify.register(fastifyCookie);
-  fastify.register(fastifySession, sessionOptions);
-
-  try {
-    fastify.after(() => {
-      routes.forEach(Route => new Route(fastify));
-    });
-    
-    fastify.listen(config.get("server.port"), "0.0.0.0", (err?: Error) => {
-      if (err) {
-        fastify.log.error(err);
-
-        return process.exit(1);
-      }
-
-      fastify.log.info(
-        `Server is listening on port ${config.get("server.port")}`
-      );
-    });
-  } catch (err) {
-    fastify.log.info('Failed to start server');
-
-    process.exit(1);
-  }
-  // createConnection().then(async (connection) => {
-    
-  // }).catch(error => {
-  //   console.log('db connection error:', error);
-  // })
-};
-main();
-
+  main();
+}).catch(error => {
+  console.log('db connection error:', error);
+});
