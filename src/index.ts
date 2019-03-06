a09728cd555774c4f0de6c63b4f4fa9e76e3a474
gsouders17 <gsouders17@gmail.com>
2019-03-06 18:59:25 -0500

    db connection updates

--- src/index.ts
+++ src/index.ts
@@ -12,64 +12,64 @@ import { createConnection } from 'typeorm';
 
 import routes from './routes';
 
-createConnection().then(async (connection) => {
-  const main = async () => {
-    const fastify = Fastify({
-      logger: config.get("server.logger")
-    });
-
-    fastify.register(fastifySwagger, {
-      exposeRoute: true,
-      swagger: {
-        consumes: ['application/json'],
-        info: {
-          description: 'Documentation for the CareLine API',
-          title: 'CareLine API',
-          version: '0.0.1'
-        },
-        produces: ['application/json'],
-        schemes: ['http'],
-      },
-    });
-
-    const redisStore = RedisStore(fastifySession);
-
-    const sessionOptions: any = {
-      cookie: {
-        secure: false
-      },
-      secret: getSessionSecret(),
-      store: new redisStore(config.get("redis"))
-    };
-
-    fastify.register(fastifyCORS, config.get("cors"));
-    fastify.register(fastifyCookie);
-    fastify.register(fastifySession, sessionOptions);
-
-    try {
-      fastify.after(() => {
-        routes.forEach(Route => new Route(fastify));
-      });
-
-      fastify.listen(config.get("server.port"), "0.0.0.0", (err ? : Error) => {
-        if (err) {
-          fastify.log.error(err);
-
-          return process.exit(1);
-        }
-
-        fastify.log.info(
-          `Server is listening on port ${config.get("server.port")}`
-        );
-      });
-    } catch (err) {
-      fastify.log.info('Failed to start server');
-
-      process.exit(1);
-    }
-
+const main = async () => {
+  const typeormConfig = {
+    instance: 'db'
   };
-  main();
-}).catch(error => {
-  console.log('db connection error:', error);
-});
+  
+  const fastify = Fastify({
+    logger: config.get("server.logger")
+  });
+
+  fastify.register(fastifySwagger, {
+    exposeRoute: true,
+    swagger: {
+      consumes: ['application/json'],
+      info: {
+        description: 'Documentation for the CareLine API',
+        title: 'CareLine API',
+        version: '0.0.1'
+      },
+      produces: ['application/json'],
+      schemes: ['http'],
+    },
+  });
+
+  const redisStore = RedisStore(fastifySession);
+
+  const sessionOptions: any = {
+    cookie: {
+      secure: false
+    },
+    secret: getSessionSecret(),
+    store: new redisStore(config.get("redis"))
+  };
+
+  fastify.register(fastifyCORS, config.get("cors"));
+  fastify.register(fastifyCookie);
+  fastify.register(fastifySession, sessionOptions);
+
+  try {
+    fastify.after(() => {
+      routes.forEach(Route => new Route(fastify));
+    });
+
+    fastify.listen(config.get("server.port"), "0.0.0.0", (err ? : Error) => {
+      if (err) {
+        fastify.log.error(err);
+
+        return process.exit(1);
+      }
+
+      fastify.log.info(
+        `Server is listening on port ${config.get("server.port")}`
+      );
+    });
+  } catch (err) {
+    fastify.log.info('Failed to start server');
+
+    process.exit(1);
+  }
+
+};
+main();
