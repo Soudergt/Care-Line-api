01eec4175a5bec9c00db0ae3fe3c80bcec5a9631
gsouders17 <gsouders17@gmail.com>
2019-03-06 17:46:08 -0500

    clinic rating entitiy

---
+++ src/Entities/clinic-rating.ts
@@ -0,0 +1,24 @@
+import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class ClinicRating {
+
+    @PrimaryGeneratedColumn()
+    ClinicRatingID: number;
+
+    @Column()
+    FKClinicID: number;
+
+    @Column()
+    StarRating: number;
+
+    @Column("text")
+    TextRating: string;
+
+    @Column()
+    TimeUpdated: Date;
+
+    @Column()
+    FKRatedByUser: number;
+
+}
