b62cda499ac520819b548cce45724727be5662d4
gsouders17 <gsouders17@gmail.com>
2019-03-06 17:23:35 -0500

    added columns for user

--- src/Entities/user.ts
+++ src/Entities/user.ts
@@ -6,4 +6,64 @@ export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Column()
+    UserType: string;
+
+    @Column()
+    UserEmail: string;
+
+    @Column()
+    NamePrefix: string;
+
+    @Column()
+    NameFirst: string;
+
+    @Column()
+    NameMiddle: string;
+
+    @Column()
+    NameLast: string;
+
+    @Column()
+    NameSuffix: string;
+
+    @Column()
+    JobTitle: string;
+
+    @Column()
+    HomeAddress: string;
+
+    @Column()
+    WorkPhone: number;
+
+    @Column()
+    MobilePhone: number;
+
+    @Column()
+    HomePhone: number;
+
+    @Column()
+    DateOfBirth: Date;
+
+    @Column()
+    FeaturesEnabled: boolean;
+
+    @Column()
+    UserPhoto: string;
+
+    @Column()
+    HeightInches: number;
+
+    @Column()
+    WeightPounds: number;
+
+    @Column()
+    BloodType: string;
+
+    @Column()
+    AllergyInfo: string;
+
+    @Column()
+    DietInfo: string;
+
 }
