2c638ebe42d848bc1342b15866fc5286d2722754
gsouders17 <gsouders17@gmail.com>
2019-03-11 19:20:54 -0400

    rating routes and services

--- src/libs/Rating/rating-service.ts
+++ src/libs/Rating/rating-service.ts
@@ -3,6 +3,19 @@ import { ClinicRating } from "../../Entities/clinic-rating";
 import { CaretakerRating } from "../../Entities/caretaker-rating";
 
 export class RatingService {
+  public async getClinicRatings(id: number) {
+    try {
+      const clinicRatingRepo = getRepository(ClinicRating);
+
+      const ratings = await clinicRatingRepo.find();    
+  
+      return ratings;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
   public async getClinicRating(id: number) {
     try {
       const clinicRatingRepo = getRepository(ClinicRating);
@@ -10,6 +23,19 @@ export class RatingService {
       const rating = await clinicRatingRepo.findOne(id);    
   
       return rating;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  public async getCaretakerRatings(id: number) {
+    try {
+      const caretakerRatingRepo = getRepository(CaretakerRating);
+
+      const ratings = await caretakerRatingRepo.find();    
+  
+      return ratings;
     } catch (err) {
       console.log(err);
       return err;
--- src/routes/Rating/rating-router.ts
+++ src/routes/Rating/rating-router.ts
@@ -6,8 +6,107 @@ import { RatingService } from '../../libs/Rating/rating-service';
 export class RatingRouter {
   constructor(fastify: FastifyInstance) {
     fastify.route({
-      handler: this.getRating,
-      url: '/rating/getRating',
+      handler: this.getCaretakerRatings,
+      url: '/rating/caretaker/getRatings',
+      method: 'GET',
+      schema: {
+        querystring: {
+          id: { type: 'number' }
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              data: {
+                additionalProperties: true,
+                ratings: {
+                  type: "array"
+                },
+                type: "object"
+              }
+            }
+          },
+          400: {
+            properties: {
+              data: { type: "object" },
+              message: { type: "string" },
+              statusCode: { type: "integer" }
+            },
+            type: "object"
+          }
+        }
+      }
+    });
+
+    fastify.route({
+      handler: this.getCaretakerRating,
+      url: '/rating/caretaker/getRating',
+      method: 'GET',
+      schema: {
+        querystring: {
+          id: { type: 'number' }
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              data: {
+                additionalProperties: true,
+                rating: {
+                  type: "array"
+                },
+                type: "object"
+              }
+            }
+          },
+          400: {
+            properties: {
+              data: { type: "object" },
+              message: { type: "string" },
+              statusCode: { type: "integer" }
+            },
+            type: "object"
+          }
+        }
+      }
+    });
+
+    fastify.route({
+      handler: this.getClinicRatings,
+      url: '/rating/clinic/getRatings',
+      method: 'GET',
+      schema: {
+        querystring: {
+          id: { type: 'number' }
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              data: {
+                additionalProperties: true,
+                ratings: {
+                  type: "array"
+                },
+                type: "object"
+              }
+            }
+          },
+          400: {
+            properties: {
+              data: { type: "object" },
+              message: { type: "string" },
+              statusCode: { type: "integer" }
+            },
+            type: "object"
+          }
+        }
+      }
+    });
+
+    fastify.route({
+      handler: this.getClinicRating,
+      url: '/rating/clinic/getRating',
       method: 'GET',
       schema: {
         querystring: {
@@ -39,11 +138,71 @@ export class RatingRouter {
     });
   }
 
-  private async getRating(request: Request, reply: Response) {
+  private async getCaretakerRatings(request: Request, reply: Response) {
     try {
       const { id } = request.query;
 
-      const rating = await new RatingService().getRating(id);
+      const ratings = await new RatingService().getCaretakerRatings(id);
+      
+      reply.code(200).send({
+        data: { ratings },
+        message: 'SUCCESS',
+        statusCode: 200
+      });
+    } catch (error) {
+      reply.code(400).send({
+        data: {},
+        message: 'ERROR',
+        statusCode: 400
+      });
+    }
+  }
+
+  private async getCaretakerRating(request: Request, reply: Response) {
+    try {
+      const { id } = request.query;
+
+      const rating = await new RatingService().getCaretakerRating(id);
+      
+      reply.code(200).send({
+        data: { rating },
+        message: 'SUCCESS',
+        statusCode: 200
+      });
+    } catch (error) {
+      reply.code(400).send({
+        data: {},
+        message: 'ERROR',
+        statusCode: 400
+      });
+    }
+  }
+
+  private async getClinicRatings(request: Request, reply: Response) {
+    try {
+      const { id } = request.query;
+
+      const ratings = await new RatingService().getClinicRatings(id);
+      
+      reply.code(200).send({
+        data: { ratings },
+        message: 'SUCCESS',
+        statusCode: 200
+      });
+    } catch (error) {
+      reply.code(400).send({
+        data: {},
+        message: 'ERROR',
+        statusCode: 400
+      });
+    }
+  }
+
+  private async getClinicRating(request: Request, reply: Response) {
+    try {
+      const { id } = request.query;
+
+      const rating = await new RatingService().getClinicRating(id);
       
       reply.code(200).send({
         data: { rating },
