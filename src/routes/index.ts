471a5ad79a86c3eed7221dcdcc4827f5877b1a68
gsouders17 <gsouders17@gmail.com>
2019-03-11 18:36:44 -0400

    removed unused routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,15 @@
 import { UserRouter } from './User/user-router';
-import { PatientRouter } from './Patient/patient-router';
 import { ScheduleRouter } from './Schedule/schedule-router';
 import { NoteRouter } from './Note/note-router';
 import { AuthRouter } from './Auth/auth-router';
-import { CaretakerRouter } from './Caretaker/caretaker-router';
 import { ClinicRouter } from './Clinic/clinic-router';
 import { FeedbackRouter } from './Feedback/feedback-router';
 
 export default [
-  PatientRouter,
   UserRouter,
   ScheduleRouter,
   NoteRouter,
   AuthRouter,
-  CaretakerRouter,
   ClinicRouter,
   FeedbackRouter
 ];
