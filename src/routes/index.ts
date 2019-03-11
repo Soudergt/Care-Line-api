e82d00b55a3d79a7c3c80249188366f01914e792
gsouders17 <gsouders17@gmail.com>
2019-03-11 18:53:12 -0400

    added new routers to module

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,17 @@
 import { UserRouter } from './User/user-router';
 import { ScheduleRouter } from './Schedule/schedule-router';
-import { NoteRouter } from './Note/note-router';
 import { AuthRouter } from './Auth/auth-router';
 import { ClinicRouter } from './Clinic/clinic-router';
 import { FeedbackRouter } from './Feedback/feedback-router';
+import { StatusRouter } from './Status/status-router';
+import { RatingRouter } from './Rating/rating-router';
 
 export default [
   UserRouter,
   ScheduleRouter,
-  NoteRouter,
   AuthRouter,
   ClinicRouter,
-  FeedbackRouter
+  FeedbackRouter,
+  StatusRouter,
+  RatingRouter
 ];
