ad24c4bfb0da460322ca1c41d4252e736d42d379
gsouders17 <gsouders17@gmail.com>
2019-03-19 18:55:27 -0400

    request session

--- src/interfaces/Request/index.ts
+++ src/interfaces/Request/index.ts
@@ -2,19 +2,19 @@ import { FastifyRequest } from "fastify";
 import { IncomingMessage } from "http";
 
 export interface Request extends FastifyRequest<IncomingMessage> {
-  // sessionStore: {
-  //   destroy: (sessionId: string, callback: (err?: Error) => void) => void
-  // },
-  // session?: {
-  //   tfid?: string;
-  //   user?: {
-  //     fn?: string;
-  //     ln?: string;
-  //     uid?: string;
-  //     fbid?: string;
-  //     img?: string
-  //   };
-  //   sessionId: string;
-  //   time?: number;
-  // };
+  sessionStore: {
+    destroy: (sessionId: string, callback: (err?: Error) => void) => void
+  },
+  session?: {
+    tfid?: string;
+    user?: {
+      fn?: string;
+      ln?: string;
+      uid?: string;
+      fbid?: string;
+      img?: string
+    };
+    sessionId: string;
+    time?: number;
+  };
 }
