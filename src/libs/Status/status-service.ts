c3444073fa9df6b429603cf21d48ab0af6fb9365
gsouders17 <gsouders17@gmail.com>
2019-03-20 19:08:06 -0400

    status routes and services

--- src/libs/Status/status-service.ts
+++ src/libs/Status/status-service.ts
@@ -4,11 +4,50 @@ import { Status } from "../../Entities/status";
 export class StatusService {
   public async getStatus(uid: string, date: string) {
     try {      
-      // const statusRepo = getRepository(Status);
+      const statusRepo = getRepository(Status);
 
-      // const status = await statusRepo.findOne();
-      const status = "test";
+      const status = await statusRepo.findOne();
+
       return status;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  public async addStatus(uid: string, status: any) {
+    try {      
+      const statusRepo = getRepository(Status);
+
+      const newStatus = await statusRepo.save(status);
+      
+      return newStatus;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  public async editStatus(uid: string, status: any) {
+    try {      
+      const statusRepo = getRepository(Status);
+
+      const updatedStatus = await statusRepo.save(status);
+      
+      return updatedStatus;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  public async deleteStatus(uid: string, status: any) {
+    try {      
+      const statusRepo = getRepository(Status);
+
+      const removedStatus = await statusRepo.remove(status);
+      
+      return removedStatus;
     } catch (err) {
       console.log(err);
       return err;
--- src/routes/Status/status-router.ts
+++ src/routes/Status/status-router.ts
@@ -38,6 +38,106 @@ export class StatusRouter {
         }
       }
     });
+
+    fastify.route({
+      handler: this.addStatus,
+      url: '/status/add',
+      method: 'POST',
+      schema: {
+        body: {
+          uid: { type: 'number' },
+          status: { type: 'object' }
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              data: {
+                additionalProperties: true,
+                newStatus: {
+                  type: "object"
+                },
+                type: "object"
+              }
+            }
+          },
+          400: {
+            properties: {
+              data: { type: "object" },
+              message: { type: "string" },
+              statusCode: { type: "integer" }
+            },
+            type: "object"
+          }
+        }
+      }
+    });
+
+    fastify.route({
+      handler: this.editStatus,
+      url: '/status/edit',
+      method: 'PUT',
+      schema: {
+        body: {
+          status: { type: 'object' }
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              data: {
+                additionalProperties: true,
+                updatedStatus: {
+                  type: "object"
+                },
+                type: "object"
+              }
+            }
+          },
+          400: {
+            properties: {
+              data: { type: "object" },
+              message: { type: "string" },
+              statusCode: { type: "integer" }
+            },
+            type: "object"
+          }
+        }
+      }
+    });
+
+    fastify.route({
+      handler: this.deleteStatus,
+      url: '/status/delete',
+      method: 'POST',
+      schema: {
+        body: {
+          status: { type: 'object' }
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              data: {
+                additionalProperties: true,
+                removedStatus: {
+                  type: "object"
+                },
+                type: "object"
+              }
+            }
+          },
+          400: {
+            properties: {
+              data: { type: "object" },
+              message: { type: "string" },
+              statusCode: { type: "integer" }
+            },
+            type: "object"
+          }
+        }
+      }
+    });
   }
 
   private async getStatus(request: Request, reply: Response) {
@@ -48,6 +148,66 @@ export class StatusRouter {
       
       reply.code(200).send({
         data: { status },
+        message: 'SUCCESS',
+        statusCode: 200
+      });
+    } catch (error) {
+      reply.code(400).send({
+        data: {},
+        message: 'ERROR',
+        statusCode: 400
+      });
+    }
+  }
+
+  private async addStatus(request: Request, reply: Response) {
+    try {
+      const { uid, status } = request.body;
+
+      const newStatus = await new StatusService().addStatus(uid, status);
+      
+      reply.code(200).send({
+        data: { newStatus },
+        message: 'SUCCESS',
+        statusCode: 200
+      });
+    } catch (error) {
+      reply.code(400).send({
+        data: {},
+        message: 'ERROR',
+        statusCode: 400
+      });
+    }
+  }
+
+  private async editStatus(request: Request, reply: Response) {
+    try {
+      const status = request.body;
+
+      const updatedStatus = await new StatusService().editStatus(status);
+      
+      reply.code(200).send({
+        data: { updatedStatus },
+        message: 'SUCCESS',
+        statusCode: 200
+      });
+    } catch (error) {
+      reply.code(400).send({
+        data: {},
+        message: 'ERROR',
+        statusCode: 400
+      });
+    }
+  }
+
+  private async deleteStatus(request: Request, reply: Response) {
+    try {
+      const status = request.body;
+
+      const removedStatus = await new StatusService().deleteStatus(status);
+      
+      reply.code(200).send({
+        data: { removedStatus },
         message: 'SUCCESS',
         statusCode: 200
       });
