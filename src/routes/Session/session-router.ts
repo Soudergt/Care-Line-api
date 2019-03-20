dc32d90f072245e6af77525f773588ca440fba53
gsouders17 <gsouders17@gmail.com>
2019-03-20 19:56:32 -0400

    check session router

---
+++ src/routes/Session/session-router.ts
@@ -0,0 +1,40 @@
+import { FastifyInstance } from "fastify";
+import { Request } from './../../interfaces/Request';
+import { Response } from './../../interfaces/Response';
+
+export class SessionRouter {
+  constructor(fastify: FastifyInstance) {
+    fastify.route({
+      handler: this.checkSession,
+      method: "GET",
+      schema: {
+        response: {
+          200: {
+            properties: {
+              valid: {
+                type: "boolean"
+              }
+            },
+            type: "object"
+          },
+          440: {
+            type: "string"
+          }
+        }
+      },
+      url: "/session/valid"
+    });
+  }
+
+  private async checkSession(request: Request, reply: Response) {
+    try {
+      if (!request.session.user) {
+        return reply.code(200).send({ valid: false });
+      }
+
+      reply.code(200).send({ valid: true });
+    } catch (error) {
+      reply.code(440).send({ valid: false });
+    }
+  }
+}
