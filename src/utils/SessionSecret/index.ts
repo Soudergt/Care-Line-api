8e9926cd32b70ead4d2b2594879ec7177faca73e
gsouders17 <gsouders17@gmail.com>
2019-03-28 13:54:17 -0400

    hid process npm version to session secret

--- src/utils/SessionSecret/index.ts
+++ src/utils/SessionSecret/index.ts
@@ -4,7 +4,7 @@ import { createHmac } from "crypto";
 export function getSessionSecret() {
   const hash = createHmac('sha512', config.get("session.salt"));
 
-  hash.update(process.env.npm_package_version);
+  // hash.update(process.env.npm_package_version);
 
   return hash.digest('hex');
 }
